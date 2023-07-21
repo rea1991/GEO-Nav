00c7346821903b5e28aac3f18050ef8da2dae42d
Andrea Raffo <andreraf@uio.no>
2023-07-21 10:13:29 +0200

    Delete copy_PBDs.sh

--- copy_PBDs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for folder in toys/*; do
-	echo ${folder}
-	echo /Users/andrea/Documents/GitHub/GEO-Nav/dataset/${folder}
-	mv ${folder}/NanoShaper_input.xyzr ${folder}/input.xyzr
-done
